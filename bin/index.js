371a8fef731d9eba8be5e6866f72ceb8a9599f9a
Harshil <ghagrawal17@gmail.com>
2022-01-15 09:56:05 +0100

    ⚡ Add chalk and boxen

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
+const chalk = require('chalk');
+const boxen = require('boxen');
+// import boxen from 'boxen';
 
-console.log(`
-Name: Harshil Agrawal
-Website: https://harshil.dev
-Twitter: https://twitter.com/harshil1712
-GitHub: https://github.com/harshil1712
-`);
+const name = chalk.white('Name') + ': ' + chalk.green('Harshil Agrawal');
+const website =
+	chalk.white('Webiste') + ': ' + chalk.green('https://harshil.dev');
+const twitter =
+	chalk.white('Twitter') +
+	': ' +
+	chalk.green('https://twitter.com/harshil1712');
+const github =
+	chalk.white('GitHub') + ': ' + chalk.green('https://github.com/harshil1712');
+
+const box = {
+	padding: 1,
+	margin: 1,
+	borderStyle: 'round',
+	borderColor: 'green',
+};
+
+const message = boxen(`${name}\n${website}\n${twitter}\n${github}`, box);
+
+console.log(message);
